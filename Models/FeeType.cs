38b8a81c19652c22ed283752fa154f33c7ae8740
pwn1kunwar <pwn1kunwar@gmail.com>
2025-05-01 18:20:06 +0545

    FeeType Model Created

---
+++ Models/FeeType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolBillingERP.Models
+{
+    public class FeeType
+    {
+        [Key]
+        public Guid FeeTypeId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
