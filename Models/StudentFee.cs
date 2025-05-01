2e02d253cc29d5e4da1c04b54758c805e3403fc7
pwn1kunwar <pwn1kunwar@gmail.com>
2025-05-01 23:47:26 +0545

    StudentFee Class added

---
+++ Models/StudentFee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolBillingERP.Models
+{
+    public class StudentFee
+    {
+        [Key]
+        public Guid StudentFeeId { get; set; }
+        public Guid StudentId { get; set; }
+        public Guid FeeTypeId { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentDate { get; set; }
+        public string FeeStatus { get; set; }
+
+    }
+}
