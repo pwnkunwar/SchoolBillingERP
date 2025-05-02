e63b8a686c822114ea7aaaab9e0cc9556e6cdc40
pwn1kunwar <pwn1kunwar@gmail.com>
2025-05-02 17:04:44 +0545

    FiscalYear Model added

---
+++ Models/FiscalYear.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolBillingERP.Models
+{
+    public class FiscalYear
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
