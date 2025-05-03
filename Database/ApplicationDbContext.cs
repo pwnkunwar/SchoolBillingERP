2830af5f2ab0eb534d9aa0d4a0d0eee71106c946
pwn1kunwar <pwn1kunwar@gmail.com>
2025-05-03 14:56:41 +0545

    moved from DbContext class to IdentityDbContext Class

--- Database/ApplicationDbContext.cs
+++ Database/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SchoolBillingERP.Models;
 
 namespace SchoolBillingERP.Database
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
