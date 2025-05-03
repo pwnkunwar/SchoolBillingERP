dd5fa3039dccb323c5f3d1a08300fac2555b47f5
pwn1kunwar <pwn1kunwar@gmail.com>
2025-05-03 14:55:59 +0545

    LoginModel added

---
+++ Models/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolBillingERP.Models
+{
+    public class LoginModel
+    {
+        
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public bool RememberMe { get; set; }
+    }
+}
