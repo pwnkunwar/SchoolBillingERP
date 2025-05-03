fd981def947c28c2a15496cb8fa6122b4fb25464
pwn1kunwar <pwn1kunwar@gmail.com>
2025-05-03 14:52:53 +0545

    Migrations Updates

---
+++ Migrations/20250502093914_Initial.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolBillingERP.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FeeTypes",
+                columns: table => new
+                {
+                    FeeTypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FeeTypes", x => x.FeeTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FiscalYears",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FiscalYears", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SchoolClasses",
+                columns: table => new
+                {
+                    ClassId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClassName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SchoolClasses", x => x.ClassId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ParentPhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClassId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SchoolClassClassId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                    table.ForeignKey(
+                        name: "FK_Students_SchoolClasses_ClassId",
+                        column: x => x.ClassId,
+                        principalTable: "SchoolClasses",
+                        principalColumn: "ClassId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Students_SchoolClasses_SchoolClassClassId",
+                        column: x => x.SchoolClassClassId,
+                        principalTable: "SchoolClasses",
+                        principalColumn: "ClassId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentFees",
+                columns: table => new
+                {
+                    StudentFeeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FeeTypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PaymentDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FeeStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FiscalYear = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Month = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentFees", x => x.StudentFeeId);
+                    table.ForeignKey(
+                        name: "FK_StudentFees_FeeTypes_FeeTypeId",
+                        column: x => x.FeeTypeId,
+                        principalTable: "FeeTypes",
+                        principalColumn: "FeeTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentFees_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentFees_FeeTypeId",
+                table: "StudentFees",
+                column: "FeeTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentFees_StudentId",
+                table: "StudentFees",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_ClassId",
+                table: "Students",
+                column: "ClassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_SchoolClassClassId",
+                table: "Students",
+                column: "SchoolClassClassId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FiscalYears");
+
+            migrationBuilder.DropTable(
+                name: "StudentFees");
+
+            migrationBuilder.DropTable(
+                name: "FeeTypes");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "SchoolClasses");
+        }
+    }
+}
