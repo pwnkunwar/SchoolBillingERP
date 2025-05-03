9b68bdbababee6c12fe94dc4f80f6dc84c31b69b
pwn1kunwar <pwn1kunwar@gmail.com>
2025-05-03 11:58:01 +0545

    added relationshiop between tbls

--- Database/ApplicationDbContext.cs
+++ Database/ApplicationDbContext.cs
@@ -5,13 +5,32 @@ namespace SchoolBillingERP.Database
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-       // public DbSet<Student> Students { get; set; }
+
+        public DbSet<Student> Students { get; set; }
         public DbSet<SchoolClass> SchoolClasses { get; set; }
         public DbSet<FeeType> FeeTypes { get; set; }
+        public DbSet<StudentFee> StudentFees { get; set; }
+        public DbSet<FiscalYear> FiscalYears { get; set; }  
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Define the relationship between Student and SchoolClass
+            modelBuilder.Entity<Student>()
+                .HasOne(s => s.SchoolClass)
+                .WithMany()
+                .HasForeignKey(s => s.ClassId);
+
+           
+            modelBuilder.Entity<StudentFee>()
+                .HasOne(sf => sf.FeeType)
+                .WithMany()
+                .HasForeignKey(sf => sf.FeeTypeId);
+        }
 
     }
 }
