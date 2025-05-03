cbb5fd8f082930aed138d0d206ce19c0c3de151d
pwn1kunwar <pwn1kunwar@gmail.com>
2025-05-03 11:59:15 +0545

    added property

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SchoolBillingERP.Models
 {
@@ -8,8 +9,14 @@ namespace SchoolBillingERP.Models
         public Guid StudentId { get; set; }
         [Required]
         public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string ParentPhoneNumber { get; set; }   
+        public Guid ClassId { get; set; }
+        public string Status { get; set; }
 
-
-
+        //Navigation Properties
+        public SchoolClass? SchoolClass { get; set; }
+        public ICollection<StudentFee> StudentFees { get; set; }
     }
 }
