1276978fd622ae85ba76120952e4c234d12ea0b2
pwn1kunwar <pwn1kunwar@gmail.com>
2025-05-03 11:58:48 +0545

    added property

--- Models/StudentFee.cs
+++ Models/StudentFee.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SchoolBillingERP.Models
 {
@@ -6,11 +8,17 @@ namespace SchoolBillingERP.Models
     {
         [Key]
         public Guid StudentFeeId { get; set; }
-        public Guid StudentId { get; set; }
         public Guid FeeTypeId { get; set; }
         public decimal Amount { get; set; }
         public string PaymentDate { get; set; }
         public string FeeStatus { get; set; }
+        public string FiscalYear { get; set; }
+        public string? Month { get; set; }
+
+        public FeeType FeeType { get; set; }
+
+        public Guid StudentId { get; set; }
+        public Student Student { get; set; }
 
     }
 }
