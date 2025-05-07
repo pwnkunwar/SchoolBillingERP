4db7bf6621016cd24cb1e280c54090eccbf9642c
pwn1kunwar <pwn1kunwar@gmail.com>
2025-05-07 15:06:51 +0545

    added Edit and Delete method

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -65,6 +65,88 @@ namespace SchoolBillingERP.Controllers
                 return RedirectToAction(nameof(Index));
             }
         }
+       
+        public IActionResult EditStudentDetails(Guid id)
+        {
+            if(id == Guid.Empty)
+            {
+                return NotFound();
+            }   
+            try
+            {
+                var student = _db.Students
+                    .Include(s => s.SchoolClass)
+                    .FirstOrDefault(s => s.StudentId == id);
+                if (student == null)
+                {
+                    return NotFound();
+                }
+                ViewData["ClassList"] = new SelectList(_db.SchoolClasses, "ClassId", "ClassName");
+                return View(student);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                return View();
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditStudentDetails(Student student)
+        {
+            if (student == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                var updateStudent = new Student()
+                { 
+                    StudentId = student.StudentId,
+                    FullName = student.FullName,
+                    Address = student.Address,
+                    Gender = student.Gender,
+                    ParentPhoneNumber = student.ParentPhoneNumber,
+                    Status = student.Status,
+                    ClassId = student.ClassId
+                };
+                _db.Students.Update(updateStudent);
+               
+                await _db.SaveChangesAsync();
+                TempData["Message"] = "Student Edited successfully!";
+                TempData["MessageType"] = "success";
+                return RedirectToAction("GetAllStudents");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return View();
+            }
+        }
+        public async Task<IActionResult> DeleteStudentDetails(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return NotFound();
+            }
+            try
+            {
+                var student = await _db.Students.FindAsync(id);
+                if (student == null)
+                {
+                    return NotFound();
+                }
+                _db.Students.Remove(student);
+                await _db.SaveChangesAsync();
+                TempData["Message"] = "Student Deleted successfully!";
+                TempData["MessageType"] = "success";
+                return RedirectToAction("GetAllStudents");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return View();
+            }
+        }
 
     }
 }
