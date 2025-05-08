07c29fcc7d8919c80b8f5ede3bf37c27bb90aca3
pwn1kunwar <pwn1kunwar@gmail.com>
2025-05-09 00:46:11 +0545

    StudentBill Class added

---
+++ Models/StudentBill.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace SchoolBillingERP.Models
+{
+    public class StudentBill
+    {
+        public int   InvoiceNumber { get; set; }
+        public string InvoiceDate { get; set; }
+        public string StudentName { get; set; }
+        public string ClassName { get; set; }
+        public string Address { get; set; }
+        public List<FeeItem> FeeItems { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal Amount { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string FiscalYear { get; set; }
+        public string ModeOfPayment { get; set; }
+        public string TotalAmountWords { get; set; }
+        public string BilledBy { get; set; }
+    }
+    public class FeeItem
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
