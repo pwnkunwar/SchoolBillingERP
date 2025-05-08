7f7f7f3bb7ee2d54f75716ba34408fa60f18799f
pwn1kunwar <pwn1kunwar@gmail.com>
2025-05-09 00:46:34 +0545

    Migrations added

---
+++ Migrations/20250508184917_StudentFeetblUpdated.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolBillingERP.Migrations
+{
+    /// <inheritdoc />
+    public partial class StudentFeetblUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FeeStatus",
+                table: "StudentFees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FeeStatus",
+                table: "StudentFees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
